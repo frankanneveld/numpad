63feb814579f6042c781a6b65d5b99a68f47409a
Frank Anneveld <anneveld@Franks-MacBook-Pro.local>
2020-09-11 08:37:14 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,17 @@ import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls: ['./app.component.scss'],
+  template:`<h2>NumPad</h2>
+            <div class="result">{{(result$ | async)}}</div>
+            <hr>
+            <div class="numpad">
+              <div *ngFor="let item of items$ | async">
+                <div class="item" (click)="onClick(item)">{{item}}</div>
+              </div>
+              <div class="item" (click)="calculate()">=</div>
+              <div class="item" (click)="result$.next('')">C</div>
+            </div>`})
 export class AppComponent {
   private num: string[] = [...Array(10)].toString().split(',').map( (r,i) => r + i);
   public items$: Observable<string[]> = of([...this.num, '-', '+', '/', '*']);
